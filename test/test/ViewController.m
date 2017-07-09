ee84c613fcbfa94801ef3af9125689671c2224b6
LHsunshine <2326055139@qq.com>
2017-07-09 11:12:08 +0800

    Login

--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -16,6 +16,28 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    CGRect screen=[[UIScreen mainScreen]bounds];
+    CGFloat labelWidth=90;
+    CGFloat labelHeight=20;
+    CGFloat labelTopView=150;
+    
+    UILabel *label=[[UILabel alloc]initWithFrame:CGRectMake((screen.size.width-labelWidth)/2, labelTopView, labelWidth, labelHeight)];
+    
+    label.text=@"hello";
+    label.textAlignment=NSTextAlignmentCenter;
+    [self.view addSubview:label];
+    
+    UIButton *button=[UIButton buttonWithType:UIButtonTypeSystem];
+    [button setTitle:@"OK" forState:UIControlStateNormal];
+    
+    CGFloat buttonWidth=60;
+    CGFloat buttonHeight=20;
+    CGFloat buttonTopView=240;
+    
+    button.frame=CGRectMake((screen.size.width-buttonWidth)/2, buttonTopView, buttonWidth, buttonHeight);
+    
+    [self.view addSubview:button];
+
     // Do any additional setup after loading the view, typically from a nib.
 }
 
